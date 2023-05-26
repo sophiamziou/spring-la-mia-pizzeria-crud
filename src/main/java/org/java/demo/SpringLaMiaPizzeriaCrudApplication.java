57fc376d953d169579de25ba60592d51a789551a
sophiamziou <sofiamziou@gmail.com>
2023-05-26 13:48:48 +0200

    first commit and entity done

---
+++ src/main/java/org/java/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+}
