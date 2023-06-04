6771a80f863311a9c0245f85f26390ce86efa0f6
sophiamziou <sofiamziou@gmail.com>
2023-06-04 22:26:05 +0200

    create without valid done

--- src/main/java/org/java/demo/controller/PizzaController.java
+++ src/main/java/org/java/demo/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,5 +56,17 @@ public class PizzaController {
 		model.addAttribute("pizze", pizze);
 		
 		return "index";
+	}
+	
+	@GetMapping("/pizza/create")
+	public String createPizza() {
+		return "pizza-create";
+	}
+	
+	@PostMapping("/pizza/create")
+	public String savePizza(@ModelAttribute Pizza pizza) {
+		
+		pizzaService.save(pizza);
+		return "redirect:/";
 	}
 }
